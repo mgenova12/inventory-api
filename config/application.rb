071ece78b8b4cac1b2a4efe271508fff10da417a
Marco <mgenova03@gmail.com>
2019-04-16 16:01:54 -0400

    update cors config

--- config/application.rb
+++ config/application.rb
@@ -28,7 +28,7 @@ module InventoryApi
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins 'http://localhost:3000', '*' #replace this url with that of your own heroku client app
-        resource '*', :headers => :any, :methods => [:get]
+        resource '*', :headers => :any, :methods => :any
       end
     end
 
