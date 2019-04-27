5bec7526c2898661f3cec9e1bf1ccabf024a4060
Marco <mgenova03@gmail.com>
2019-04-27 10:59:52 -0400

    graphql to staging

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   
-  if Rails.env.development?
+  if Rails.env.development? || Rails.env.staging?
     mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
   end
 
   post "/graphql", to: "graphql#execute"
   
-  root to: 'welcome#index'
+  root to: 'welcome#index'  
 end
