5407b7e3134d0f6554ff4e6c6e8803a2672892e0
Marco <mgenova03@gmail.com>
2019-05-01 11:22:42 -0400

    update products query

--- app/graphql/types/product_type.rb
+++ app/graphql/types/product_type.rb
@@ -2,11 +2,11 @@ module Types
   class ProductType < Types::BaseObject
     field :id, ID, null: true
     field :name, String, null: true
-    field :case_quantity, Integer, null: true
-    field :price, Float, null: true
-    field :amount_in_stock, Integer, null: true
-    field :distributor, DistributorType, null: true
-    field :measurement, MeasurementType, null: true
-    field :product_type, ProductTypeType, null: true
+    # field :case_quantity, Integer, null: true
+    # field :price, Float, null: true
+    # field :amount_in_stock, Integer, null: true
+    # field :distributor, DistributorType, null: true
+    # field :measurement, MeasurementType, null: true
+    # field :product_type, ProductTypeType, null: true
   end
 end
--- app/graphql/types/product_type_type.rb
+++
@@ -1,6 +0,0 @@
-module Types
-  class ProductTypeType < Types::BaseObject
-    field :id, ID, null: true
-    field :name, String, null: true
-  end
-end
