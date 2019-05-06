eed27f37b35404f7d16afb76eec4e051c391a66f
Marco <mgenova03@gmail.com>
2019-05-06 15:21:45 -0400

    add distrubtor mutation

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,5 +1,6 @@
 module Types
   class MutationType < Types::BaseObject
     field :create_store, mutation: Mutations::CreateStore
+    field :create_distributor, mutation: Mutations::CreateDistributor
   end
 end
