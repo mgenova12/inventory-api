e99a2c28718f0a133af8a39d255b2fff6c425964
Marco <mgenova03@gmail.com>
2019-05-06 20:02:25 -0400

    add dropdown queries

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -3,5 +3,8 @@ module Types
     field :products, resolver: Queries::Products
     field :stores, resolver: Queries::Stores
     field :store_types, resolver: Queries::StoreTypes
+    field :distributors, resolver: Queries::Distributors
+    field :categories, resolver: Queries::Categories
+    field :count_bies, resolver: Queries::CountBies
   end
 end
