36dc4f74840efd0efb2c6c0772c00a3e8f3f9904
Marco <mgenova03@gmail.com>
2020-07-13 18:33:11 -0400

    cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,12 +2,12 @@
 # Dotenv::Railtie.load
 # Avoid CORS issues when API is called from the frontend app.
 # Handle Cross-Origin Resource Sharing (CORS) in order to accept cross-origin AJAX requests.
-
+Dotenv::Railtie.load
 # Read more: https://github.com/cyu/rack-cors
 
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins ENV['CLIENT_URL'] #replace this url with that of your own heroku client app
-#     resource '*', :headers => :any, :methods => :any, :credentials => true
-#   end
-# end
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins ENV['CLIENT_URL'] #replace this url with that of your own heroku client app
+    resource '*', :headers => :any, :methods => :any, :credentials => true
+  end
+end
