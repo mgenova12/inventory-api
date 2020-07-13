c01d4acbe597fe275cf087422b22065e7a91af34
Marco <mgenova03@gmail.com>
2020-07-13 08:51:05 -0400

    config

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,10 @@ module InventoryApi
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-
+    config.action_dispatch.default_headers = {
+      'Access-Control-Allow-Origin' => ENV['CLIENT_URL'],
+      'Access-Control-Request-Method' => %w{GET POST OPTIONS}.join(",")
+    } 
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,13 +1,13 @@
 # Be sure to restart your server when you modify this file.
-Dotenv::Railtie.load
+# Dotenv::Railtie.load
 # Avoid CORS issues when API is called from the frontend app.
 # Handle Cross-Origin Resource Sharing (CORS) in order to accept cross-origin AJAX requests.
 
 # Read more: https://github.com/cyu/rack-cors
 
-Rails.application.config.middleware.insert_before 0, Rack::Cors do
-  allow do
-    origins ENV['CLIENT_URL'] #replace this url with that of your own heroku client app
-    resource '*', :headers => :any, :methods => :any, :credentials => true
-  end
-end
+# Rails.application.config.middleware.insert_before 0, Rack::Cors do
+#   allow do
+#     origins ENV['CLIENT_URL'] #replace this url with that of your own heroku client app
+#     resource '*', :headers => :any, :methods => :any, :credentials => true
+#   end
+# end
