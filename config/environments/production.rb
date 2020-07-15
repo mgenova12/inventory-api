c86a873d41e2d2c83f2af98214fd19948428f7ed
Marco <mgenova03@gmail.com>
2020-07-14 21:18:34 -0400

    config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  config.allowed_cors_origins = ENV['CLIENT_URL']
   # Code is not reloaded between requests.
   config.cache_classes = true
 
